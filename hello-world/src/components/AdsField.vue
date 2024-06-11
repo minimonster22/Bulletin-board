194ad8ed4f54c61242445b2c782113b20c7e79b9
minimonster22 <tamaramonstr@gmail.com>
2024-06-12 02:07:11 +0300

    feat: add a table with cards, sorted by category, location, date of placement and price

--- hello-world/src/components/AdsField.vue
+++ hello-world/src/components/AdsField.vue
@@ -1,50 +1,32 @@
 <template>
   <div>
+    <div>
+      <div>Фильтрация по цене:</div>
+      <div>
+        <InputNumber v-model="minPrice" :controls="false" placeholder="Минимальная цена" />
+        <span> - </span>
+        <InputNumber v-model="maxPrice" :controls="false" placeholder="Максимальная цена" />
+        <Button @click="applyPriceFilter" label="Применить" />
+      </div>
+    </div>
     <!-- DataView компонент -->
     <div class="card">
-      <DataView :value="ads" :layout="layout" paginator :rows="5">
-        <template #header>
-          <div class="flex justify-content-end">
-            <DataViewLayoutOptions v-model="layout" />
-          </div>
-        </template>
-        <template #list="slotProps">
-          <div class="grid-list">
-            <div v-for="(item, index) in slotProps.items" :key="index" class="col-12">
-              <div class="list-item flex flex-column sm:flex-row sm:align-items-center p-4 gap-3" :class="{ 'border-top-1 surface-border': index !== 0 }">
-                <div class="md:w-10rem relative">
-                  <img class="block xl:block mx-auto border-round w-full" :src="getImageUrl(item.image)" :alt="item.title"/>
-                </div>
-                <div class="flex flex-column md:flex-row justify-content-between md:align-items-center flex-1 gap-4">
-                  <div class="flex flex-row md:flex-column justify-content-between align-items-start gap-2">
-                    <div>
-                      <span class="font-medium text-secondary text-sm">{{ item.location }}</span>
-                      <div class="text-lg font-medium text-900 mt-2">{{ item.title }}</div>
-                    </div>
-                    <div class="surface-100 p-1" style="border-radius: 30px">
-                      <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
-                        <span class="text-900 font-medium text-sm">{{ item.category }}</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="flex flex-column md:align-items-end gap-5">
-                    <span class="text-xl font-semibold text-900">{{ item.price }} Br</span>
-                    <div class="flex flex-row-reverse md:flex-row gap-2">
-                      <Button icon="pi pi-shopping-cart" label="Купить" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" class="flex-auto md:flex-initial white-space-nowrap"></Button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </template>
+      <div class="flex justify-content-end">
+        <DataViewLayoutOptions v-model="layout"/>
+        <Dropdown v-model="sortKey" :options="sortOptions" optionLabel="label" placeholder="Сортировать"
+                  @change="onSortChange"/>
+      </div>
+
+      <DataView v-if="layout === 'grid'" :value="ads" layout="grid" paginator :rows="5" :sortOrder="sortOrder"
+                :sortField="sortField">
         <template #grid="slotProps">
           <div class="grid grid-nogutter">
             <div v-for="(item, index) in slotProps.items" :key="index" class="col-12 sm:col-6 md:col-4 xl:col-4 p-2">
               <div class="grid-item p-4 border-1 surface-border surface-card border-round flex flex-column">
                 <div class="surface-50 flex justify-content-center border-round p-3">
                   <div class="relative mx-auto">
-                    <img class="block xl:block mx-auto border-round w-full" :src="getImageUrl(item.image)" :alt="item.title"/>
+                    <img class="block xl:block mx-auto border-round w-full" :src="getImageUrl(item.image)"
+                         :alt="item.title"/>
                   </div>
                 </div>
                 <div class="pt-4">
@@ -54,7 +36,8 @@
                       <div class="text-lg font-medium text-900 mt-1">{{ item.title }}</div>
                     </div>
                     <div class="surface-100 p-1" style="border-radius: 30px">
-                      <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
+                      <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2"
+                           style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
                         <span class="text-900 font-medium text-sm">{{ item.category }}</span>
                       </div>
                     </div>
@@ -62,15 +45,59 @@
                   <div class="flex flex-column gap-4 mt-4">
                     <span class="text-2xl font-semibold text-900">{{ item.price }} Br</span>
                     <div class="flex gap-2">
-                      <Button icon="pi pi-shopping-cart" label="Купить" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" class="flex-auto white-space-nowrap"></Button>
+                      <Button icon="pi pi-shopping-cart" label="Купить"
+                              :disabled="item.inventoryStatus === 'OUTOFSTOCK'"
+                              class="flex-auto white-space-nowrap"></Button>
                     </div>
                   </div>
+                  <div class="text-lg font-medium text-900 mt-1">{{ item.date_posted }}</div>
                 </div>
               </div>
             </div>
           </div>
         </template>
       </DataView>
+
+      <DataTable v-if="layout === 'list'" v-model:filters="filters" filterDisplay="row" :value="ads" paginator
+                 :rows="10" :sortOrder="sortOrder" :sortField="sortField"
+                 :globalFilterFields="['title', 'location', 'category', 'date_posted']">
+        <Column field="image" header=" ">
+          <template #body="slotProps">
+            <img :src="getImageUrl(slotProps.data.image)" :alt="slotProps.data.title" width="50"/>
+          </template>
+        </Column>
+        <Column field="title" header="Название" :showFilterMenu="false">
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText v-model="filterModel.value" @input="filterCallback()" placeholder="Искать по названию"/>
+          </template>
+        </Column>
+        <Column field="location" header="Место" :showFilterMenu="false">
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText v-model="filterModel.value" @input="filterCallback()" placeholder="Искать по городу"/>
+          </template>
+        </Column>
+        <Column field="category" header="Категория" :showFilterMenu="false">
+          <template #body="slotProps">
+            {{ slotProps.data.category }}
+          </template>
+          <template #filter="{ filterModel, filterCallback }">
+            <Dropdown v-model="filterModel.value" @change="filterCallback()" :options="categoryOptions"
+                      optionLabel="label" optionValue="value" placeholder="Искать по категории"/>
+          </template>
+        </Column>
+        <Column field="price" header="Цена">
+          <template #body="{ data }">
+            <span>{{ data.price }} Br</span>
+          </template>
+        </Column>
+        <Column header=" ">
+          <template #body="slotProps">
+            <Button icon="pi pi-shopping-cart" label="Купить"/>
+          </template>
+        </Column>
+        <Column field="date_posted" header="Дата размещения">
+        </Column>
+      </DataTable>
     </div>
   </div>
 </template>
@@ -80,30 +107,77 @@ import axios from 'axios';
 import DataView from 'primevue/dataview';
 import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions';
 import Button from 'primevue/button';
+import Dropdown from 'primevue/dropdown';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import InputText from 'primevue/inputtext';
+import {FilterMatchMode} from 'primevue/api';
 
 export default {
   name: "AdsField",
   components: {
     DataView,
     DataViewLayoutOptions,
-    Button
+    Dropdown,
+    Button,
+    DataTable,
+    Column,
+    InputText,
   },
   data() {
     return {
-      ads: [], // Загружаемые данные
-      layout: 'list' // Начальное значение для layout
+      ads: [],
+      layout: 'list',
+      sortKey: null,
+      sortOrder: null,
+      sortField: null,
+      minPrice: null,
+      maxPrice: null,
+      filters: {
+        global: {value: null, matchMode: FilterMatchMode.CONTAINS},
+        title: {value: null, matchMode: FilterMatchMode.CONTAINS},
+        location: {value: null, matchMode: FilterMatchMode.CONTAINS},
+        category: {value: null, matchMode: FilterMatchMode.CONTAINS},
+        price: {value: null, matchMode: FilterMatchMode.EQUALS},
+        date_posted: {value: null, matchMode: FilterMatchMode.EQUALS},
+      },
+      categoryOptions: [],
+      sortOptions: [
+        {label: 'Сначала дорогие', value: '!price'},
+        {label: 'Сначала дешевые', value: 'price'},
+        {label: 'Сначала новые', value: 'date_posted'},
+        {label: 'Сначала старые', value: '!date_posted'}
+      ]
     };
   },
   methods: {
     getImageUrl(image) {
-      // Используем require для динамического импорта изображения
       return require(`@/assets/jpg/${image}`);
+    },
+    fetchCategoryOptions() {
+      const categories = [...new Set(this.ads.map(ad => ad.category))];
+      this.categoryOptions = categories.map(category => ({label: category, value: category}));
+    },
+    onSortChange(event) {
+      const value = event.value.value;
+      const sortValue = event.value;
+
+      if (value.indexOf('!') === 0) {
+        this.sortOrder = -1;
+        this.sortField = value.substring(1, value.length);
+        this.sortKey = sortValue;
+      } else {
+        this.sortOrder = 1;
+        this.sortField = value;
+        this.sortKey = sortValue;
+      }
     }
   },
   mounted() {
     axios.get('/ad-data.json')
         .then(response => {
           this.ads = response.data;
+          this.fetchCategoryOptions();
         })
         .catch(error => {
           console.error("Ошибка при загрузке данных: ", error);
--- hello-world/src/components/MainBoard.vue
+++ hello-world/src/components/MainBoard.vue
@@ -1,31 +1,25 @@
 <template>
-  <SearchLine/>
+
   <div class="main-container">
-    <FilterLine/>
+
     <div class="ads-container">
-      <SortingLine/>
       <AdsField/>
     </div>
-    <AddAds/>
+
   </div>
 </template>
 
 <script>
 
-import SearchLine from "@/components/SearchLine.vue";
-import SortingLine from "@/components/SortingLine.vue";
-import FilterLine from "@/components/FilterLine.vue";
-import AddAds from "@/components/AddAds.vue";
+
 import AdsField from "@/components/AdsField.vue";
 
 export default {
   name: 'MainBoard',
   components: {
-    SearchLine,
-    SortingLine,
-    FilterLine,
+
     AdsField,
-    AddAds
+
   }
 }
 
@@ -36,6 +30,7 @@ export default {
 .main-container {
   display: flex;
   flex-direction: row;
+  justify-content: center;
 }
 
 .ads-container {
